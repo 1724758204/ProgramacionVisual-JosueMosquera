6491d80e73e2fb2769632e73c62ae6fa00ebec27
jag-mosquera <jag.mosquera@yavirac.edu.ec>
2021-07-04 21:16:47 -0500

    inyeccion con unity

---
+++ InyeccionDependencias/InyeccionDependencias/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Unity;
+
+namespace InyeccionDependencias
+{
+    class Program
+    {
+        
+        static StockManage stockManage;
+        static void Main(string[] args)
+        {
+            Init();
+            //StockManage stockManage = new StockManage(new Stock());
+            Console.WriteLine("Comprobando stock");
+            Console.WriteLine(stockManage.MakeStock(new Entidades.Producto { NombreProducto = "lapiz", PrecioProducto = 0.75 }));
+           // stockManage = new StockManage(new StockOnline());
+            Console.WriteLine(stockManage.MakeStock(new Entidades.Producto { NombreProducto = "manzana", PrecioProducto = 0.50 }));
+
+
+
+        }
+        static void Init()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IStock, Stock>();
+            stockManage = container.Resolve<StockManage>();
+        }
+        
+    }
+}
---
+++ InyeccionDependencias/InyeccionDependencias/Stock.cs
@@ -0,0 +1,17 @@
+﻿using InyeccionDependencias.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDependencias
+{
+    class Stock : IStock
+    {
+        public string disponibleStock(Producto producto)
+        {
+            return $"el producto {producto.NombreProducto} esta disponible y el precio es {producto.PrecioProducto}";
+        }
+    }
+}
