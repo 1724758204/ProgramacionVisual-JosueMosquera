b9a5aaa4bad3913a9f768df9da92e2d412e7121f
jag-mosquera <jag.mosquera@yavirac.edu.ec>
2021-08-02 15:55:27 -0500

    a

---
+++ ProyectoIntegrador/ModuloNotificacionesPI/Entities/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuloNotificacionesPI.Entities
+{
+    public class Producto
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public float Precio { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        [ForeignKey("Usuario")]
+        [DisplayName("Usuario")]
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+    
+    }
+}
---
+++ ProyectoIntegrador/ModuloNotificacionesPI/Entities/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuloNotificacionesPI.Entities
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public Producto Producto { get; set; }
+    }
+}
